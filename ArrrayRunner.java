be30630586c637dc1cd2da74581470a6fbbb8265
Pradeep-SL <pradeep99219@gmail.com>
2023-01-06 13:21:34 +0530

    added linked list

---
+++ ArrrayRunner.java
@@ -0,0 +1,21 @@
+package com.jso.LinkedList;
+
+public class ArrrayRunner {
+
+	public static void main(String[] args) 
+	{
+		MyLinkedlist l=new MyLinkedlist();
+		l.add(10);
+		l.add(20);
+		l.add(30);
+		l.add(40);
+//		l.remove(0);
+		for (int i=0;i<l.size(); i++)
+		{
+			System.out.println(l.get(i));
+		}
+		System.out.println(l);
+		
+	}
+
+}
